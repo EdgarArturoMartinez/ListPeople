42889c7fa205923e14f0a1d12a6d97f7f1755af1
Arturo Martinez <edgar.arturo.martinez@gmail.com>
2023-05-21 12:29:32 -0500

    FirstCommit Research Directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Persona } from './persona.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ListPeople';
+  title = 'People List';
+  people: Persona[] = [new Persona('Sara', 'Martinez'), new Persona('Rafael', 'Martinez')];
+  nameInput: string = '';
+  lastNameInput: string = '';
+
+  addPerson(){
+    let person1 = new Persona(this.nameInput, this.lastNameInput);
+    this.people.push(person1);
+  }
 }
---
+++ src/app/persona.model.ts
@@ -0,0 +1,13 @@
+export class Persona{
+    /*
+    name: string = '';
+    lastName: string = '';
+
+    constructor(name: string, lastName: string){
+        this.name = name;
+        this.lastName = lastName;
+    }
+    */
+   /*BEST PRACTICE*/ 
+   constructor(public name: string, public lastName: string){}
+}
